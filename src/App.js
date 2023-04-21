41b2a1a613442310d37af33becff7d276d055123
truongnn-fv <truong_nn@flinters.vn>
2023-04-21 17:22:26 +0700

    thay ten cho order history component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Cart from './common/Cart/Cart';
 import CategoryPage from './components/shops/CategoryPage';
 import AboutUs from './components/about/about';
 import MakeOrderPage from './pages/checkout/MakeOrderPage';
-import OrderPage from './pages/order/OrderPage';
 
 import './infra/http';
 import 'react-toastify/dist/ReactToastify.css';
@@ -41,11 +40,10 @@ function App() {
             <Route path="/" exact component={Pages} />
             <Route path="/cart" exact component={Cart} />
             <Route path="/checkout" exact component={MakeOrderPage} />
-            <Route path="/order" exact component={OrderPage} />
             <Route path="/product" exact component={ProductPage} />
             <Route path="/product/:id" component={ProductDetail} />
             <Route path="/category/:id" component={CategoryPage} />
-            <Route path="/order-history" component={OrderHistoryPage} />
+            <Route path="/order" component={OrderHistoryPage} />
             <Route path="/user" exact component={User} />
             <Route path="/about" exact component={AboutUs} />
             <Route path="/admin/dashboard" component={Dashboard} />
