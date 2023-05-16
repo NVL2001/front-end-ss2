c7fb567424295d155128bed63f7dda5c1fae4bf7
NamTruong2001 <namtruongcun2001@gmail.com>
2023-05-16 22:56:31 +0700

    add update user form

--- src/infra/http/index.js
+++ src/infra/http/index.js
@@ -5,6 +5,7 @@ axios.defaults.baseURL = 'http://localhost:8080/api';
 axios.defaults.headers = {
   'content-type': 'application/json',
   'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '[GET, POST, PUT, DELETE]'
 };
 
 const token = localStorage.getItem('jwt');
